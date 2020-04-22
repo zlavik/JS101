4e0b6d28a41090d03a7b7c9a52aaed40fc248c0d
Zlavik <kivalz@live.com>
2020-04-22 16:20:23 -0700

    improved from bonus and from code review

--- lesson_2/refactoringCalculator.js
+++ lesson_2/refactoringCalculator.js
@@ -1,91 +1,97 @@
 const readline = require('readline-sync');
-
-function prompt(message) {
+const MESSAGES = require('./calculator_messages.json');
+const VALID_OPERATORS = ['+', '-', '*', '/', '^', '%'];
+const VALID_RESPONSES = ['y', 'Y', 'n', 'N'];
+var CALCULATOR_ON_OFF = 'ON';
+const LANGUAGE = 'en';
+// Function to return message in correct language
+function messages(message, lang = 'en') {
+  return MESSAGES[lang][message];
+}
+//message in correct language
+function prompt(key) {
+  let message = messages(key, LANGUAGE);
   console.log(`=> ${message}`);
 }
-
+//making sure numbers are valid
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 console.clear();
+//Main calculator
+prompt('welcomeMsg');
+do {
+  prompt('firstNumber');
+  let number1 = readline.question();
+  while (invalidNumber(number1)) {
+    prompt("invaildNumber");
+    number1 = readline.question();
+  }
+  console.clear();
 
-prompt("Calculator has been initated..\n");
+  prompt('operation');
+  let operation = readline.question();
+  while (operation.trimStart() === '' || !VALID_OPERATORS.includes(operation)) {
+    prompt(`invaildOperation`);
+    operation = readline.question();
+  }
+  console.clear();
 
-prompt('Enter the first number: ');
-let number1 = readline.question();
+  prompt('secondNumber');
+  let number2 = readline.question();
+  while (invalidNumber(number2)) {
+    prompt("invaildNumber");
+    number2 = readline.question();
+  }
+  console.clear();
 
-while (invalidNumber(number1)) {
-  prompt("Please enter a valid number!\n");
-  number1 = readline.question();
-}
-console.clear();
+  let result;
+  switch (operation) {
+    case "+":
+      result = Number(number1) + Number(number2);
+      break;
+    case '-':
+      result = Number(number1) - Number(number2);
+      break;
+    case '*':
+      result = Number(number1) * Number(number2);
+      break;
+    case '/':
+      result = Number(number1) / Number(number2);
+      break;
+    case '^':
+      result = Number(number1) ** Number(number2);
+      break;
+    case '%':
+      result = Number(number1) % Number(number2);
+      break;
+    default:
+      console.log("Error invaild opperation!");
+      break;
+  }
+  //incase of errors and printing final result
+  if (result === undefined || isNaN(result)) {
+    console.log("Error!");
+  } else {
+    prompt("FinishCalc");
+    console.log(`\n${number1} ${operation} ${number2} = ${result}\n`);
+  }
 
-prompt("Enter one of the following:");
-console.log('\n1) +\n2) -\n3) *\n4) /\n5) ^\n6) %\n');
-let operation = readline.question();
-while (!['1', '2', '3', '4', '5' , '6'].includes(operation)) {
-  prompt("Must choose 1, 2, 3, 4, 5, or 6\n");
-  operation = readline.question();
-}
-console.clear();
-
-prompt('Enter the second number: ');
-let number2 = readline.question();
-while (invalidNumber(number2)) {
-  prompt("Please enter a valid number!\n");
-  number2 = readline.question();
-}
-console.clear();
-
-let sum;
-switch (operation) {
-  case "1":
-    operation = "+";
-    sum = Number(number1) + Number(number2);
-    break;
-  case '2':
-    operation = "-";
-    sum = Number(number1) - Number(number2);
-    break;
-  case '3':
-    operation = "*";
-    sum = Number(number1) * Number(number2);
-    break;
-  case '4':
-    operation = "/";
-    if (number1 === 0) {
-      sum = 0;
-    } else if (number2 === 0) {
-      console.log("Cannot divide by 0‬");
-    } else {
-      sum = Number(number1) / Number(number2);
-    }
-    break;
-  case '5':
-    operation = "^";
-    if (number1 === 0) {
-      sum = 0;
-    } else {
-      sum = Number(number1) ** Number(number2);
-    }
-    break;
-  case '6':
-    operation = "%";
-    if (number2 === 0 ) {
-      sum = number1;
-    } else {
-      sum = Number(number1) % Number(number2);
-    }
-    break;
-  default:
-    console.log("Error invaild opperation!");
-    break;
-}
-
-if (sum === undefined || isNaN(sum)) {
-  console.log("Error!");
-} else {
-  console.log("Calculating...");
-  console.log(`\n${number1} ${operation} ${number2} = ${sum}\n`);
-  console.log('Exiting calculator..');
-}
+  prompt('continue');
+  let anotherCalculation = readline.question();
+  while (anotherCalculation.trimStart() === '' ||
+       !VALID_RESPONSES.includes(anotherCalculation)) {
+    prompt('anotherCalcInvalid');
+    anotherCalculation = readline.question();
+  }
+  switch (anotherCalculation.toUpperCase()) {
+    case 'Y':
+      CALCULATOR_ON_OFF = 'ON';
+      break;
+    default:
+      CALCULATOR_ON_OFF = 'OFF';
+      break;
+  }
+  console.clear();
+} while (CALCULATOR_ON_OFF === 'ON');
+// end of calculator code
