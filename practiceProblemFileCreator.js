c4d1f92fd195248c7e9e79c413013f5870a35485
Zlavik <kivalz@live.com>
2020-05-27 13:50:29 -0700

    added practice problem file creator

--- practiceProblemFileCreator.js
+++ practiceProblemFileCreator.js
@@ -2,10 +2,10 @@ var fs = require('fs');
 let rlSync = require('readline-sync');
 console.clear();
 
-
 let lessonOn = Number(rlSync.question('What lesson are you on? lesson_'));
 
 let numQuestions = Number(rlSync.question('How many questions are there?: '));
+
 function createBlankJsFiles (num) {
   for (let index = 1; index <= num; index += 1) {
     fs.appendFile(`lesson_${lessonOn}/question${index}.js`, `// Question ${index}:\n// `, function (err) {
